5851b58b0506452f5311bbd6c9f513f849fd6c9a
Julian Geywitz <geywitz@sipgate.de>
2020-08-05 09:57:21 +0200

    First commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM debian:10
+
+# LABEL version="0.1"
+# LABEL "com.github.actions.name"="Flatpak Builder"
+# LABEL "com.github.actions.description"="Build your flatpak project"
+# LABEL "com.github.actions.icon"="package"
+# LABEL "com.github.actions.color"="blue"
+
+# LABEL "repository"="https://github.com/bilelmoussaoui/flatpak-github-actions"
+# LABEL "homepage"="https://github.com/bilelmoussaoui/flatpak-github-actions"
+# LABEL "maintainer"="Bilal Elmoussaoui<bil.elmoussaoui@gmail.com>"
+
+# Setup Flatpak
+RUN apt update
+RUN apt install -y ganeti ganeti-os-noop net-tools
+RUN apt clean
+
+ADD entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -e
+
+echo "Update /etc/hosts"
+cat << EOF >> /etc/hosts
+192.0.2.1 cluster
+192.0.2.10 node1
+192.0.2.11 node2
+192.0.2.12 node3
+192.0.2.13 node4
+192.0.2.14 node5
+192.0.2.100 instance1
+192.0.2.101 instance2
+192.0.2.102 instance3
+192.0.2.103 instance4
+192.0.2.104 instance5
+192.0.2.105 instance6
+192.0.2.106 instance7
+192.0.2.107 instance8
+192.0.2.108 instance9
+192.0.2.109 instance10
+EOF
+
+echo "Setup interfaces"
+ip link add gnt type dummy
+ip link set dev gnt up
+
+echo "Init Cluster"
+mkdir /opt/ganeti-vcluster
+cd /usr/lib/ganeti/tools/
+./vcluster-setup -E -c 5 -n gnt /opt/ganeti-vcluster
+cd /opt/ganeti-vcluster && node1/cmd gnt-cluster init --no-etc-hosts \
+    --no-ssh-init --master-netdev=lo \
+    --enabled-disk-templates=diskless --enabled-hypervisors=fake \
+    --ipolicy-bounds-specs=min:disk-size=0,cpu-count=1,disk-count=0,memory-size=1,nic-count=0,spindle-use=0/max:disk-size=1048576,cpu-count=8,disk-count=16,memory-size=32768,nic-count=8,spindle-use=12 \
+    cluster
+
+chown root:root /opt/ganeti-vcluster/node1/var/run/ganeti/*.pid
+
+echo "Start Cluster"
+./start-all
+
+echo "Generate SSH Key"
+ssh-keygen -f /root/.ssh/id_rsa -N ""
+cat /root/.ssh/id_rsa.pub > /root/.ssh/authorized_keys
+service ssh start
+ssh-keyscan cluster >> /root/.ssh/known_hosts
+
+echo "Add four more nodes to the cluster..."
+./node1/cmd gnt-node add --no-ssh-key-check node2
+./node1/cmd gnt-node add --no-ssh-key-check node3
+./node1/cmd gnt-node add --no-ssh-key-check node4
+./node1/cmd gnt-node add --no-ssh-key-check node5
+
+# add fake instances
+echo "Create a bunch of instances..."
+./node1/cmd gnt-instance add -t diskless --no-ip-check --no-name-check --no-install -o noop --no-nics homer
+./node1/cmd gnt-instance add -t diskless --no-ip-check --no-name-check --no-install -o noop --no-nics bart
+./node1/cmd gnt-instance add -t diskless --no-ip-check --no-name-check --no-install -o noop --no-nics marge
+./node1/cmd gnt-instance add -t diskless --no-ip-check --no-name-check --no-install -o noop --no-nics lisa
+./node1/cmd gnt-instance add -t diskless --no-ip-check --no-name-check --no-install -o noop --no-nics burns
+./node1/cmd gnt-instance add -t diskless --no-ip-check --no-name-check --no-install -o noop --no-nics milhouse
+./node1/cmd gnt-instance add -t diskless --no-ip-check --no-name-check --no-install -o noop --no-nics moe
+./node1/cmd gnt-instance add -t diskless --no-ip-check --no-name-check --no-install -o noop --no-nics smithers
+
+echo "Restart RAPI"
+pkill ganeti-rapi
+export GANETI_ROOTDIR=/opt/ganeti-vcluster//node1
+export HOME=/var/lib/ganeti
+export GANETI_HOSTNAME=node1
+echo "gnt-cc {HA1}2ac2878ee230f34cd08e5f95ccc0e664 write" > /opt/ganeti-vcluster/node1/var/lib/ganeti/rapi/users
+su --preserve-environment -s /bin/bash -c "/usr/bin/python /usr/sbin/ganeti-rapi --debug -b 0.0.0.0 -f" gnt-rapi
+
+echo
+echo "You can now access the RAPI at https://gnt-cc:gnt-cc@${HOSTNAME}:5080/"
+echo

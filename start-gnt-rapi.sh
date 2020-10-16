ee90cf8b5a93d6d9008d500621332b002451adfe
Julian Geywitz <geywitz@sipgate.de>
2020-10-16 09:42:36 +0200

    Gracefully shutdown the cluster when the container stops

--- start-gnt-rapi.sh
+++ start-gnt-rapi.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-su --preserve-environment -s /bin/bash -c '/usr/bin/python /usr/sbin/ganeti-rapi --debug -b 0.0.0.0 -f' gnt-rapi
+cleanup() {
+    echo "Stopping Cluster..."
+    /opt/ganeti-vcluster/stop-all
+}
+
+trap 'true' SIGTERM
+
+su --preserve-environment -s /bin/bash -c '/usr/bin/python /usr/sbin/ganeti-rapi --debug -b 0.0.0.0 -f' gnt-rapi &
+
+wait $!
+
+cleanup
